e9ffe8a2b2ecf59e8c286939db908eb4bb7763e4
Lv <2256334253@qq.com>
2026-07-27 11:00:32 +0800

    Refactor application identifiers and display names across platforms
    Update the application namespace and identifiers from "inknest_notes" to "inknestnotes" for consistency. Modify display names in Android, iOS, Linux, macOS, and Windows configurations to "InkNest Notes" for improved branding. Ensure all relevant files reflect these changes to maintain a cohesive identity across the application.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.inknest_notes"
+    namespace = "com.example.inknestnotes"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.inknest_notes"
+        applicationId = "com.example.inknestnotes"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = maxOf(23, flutter.minSdkVersion)
--- android/app/src/main/kotlin/com/example/inknestnotes/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/inknestnotes/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.inknest_notes
+package com.example.inknestnotes
 
 import io.flutter.embedding.android.FlutterActivity
 
